7b94b1ed4a5cccc132946cb2cce5880cde713db8
Eric Nguyen <ericnguyen@Erics-MBP.hsd1.co.comcast.net>
2019-09-22 23:11:10 -0600

    Fixed cars

--- db/migrate/20190923050630_create_cars.rb
+++ db/migrate/20190923050630_create_cars.rb
@@ -1,8 +1,8 @@
 class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
-      t.reference :make
-      t.reference :model
+      t.references :make
+      t.references :model
       t.string :vin
 
       t.timestamps
