ba827132814d0bc7d5b1783e3bdf972bd7a5263f
Eric Nguyen <42782243+GotamaHuy@users.noreply.github.com>
2019-12-16 12:55:03 -0700

    Fixed errors

--- db/migrate/20191213223953_create_cars.rb
+++ db/migrate/20191213223953_create_cars.rb
@@ -1,8 +1,8 @@
 class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
-      t.references :make
-      t.references :model
+      t.string :model
+      t.references :make, foreign_key: true
       t.string :vin
 
       t.timestamps
--- test/system/cars_test.rb
+++ test/system/cars_test.rb
@@ -14,7 +14,7 @@ class CarsTest < ApplicationSystemTestCase
     visit cars_url
     click_on "New Car"
 
-    fill_in "Make", with: @car.make
+    fill_in "Make", with: @car.make_id
     fill_in "Model", with: @car.model
     fill_in "Vin", with: @car.vin
     click_on "Create Car"
@@ -27,7 +27,7 @@ class CarsTest < ApplicationSystemTestCase
     visit cars_url
     click_on "Edit", match: :first
 
-    fill_in "Make", with: @car.make
+    fill_in "Make", with: @car.make_id
     fill_in "Model", with: @car.model
     fill_in "Vin", with: @car.vin
     click_on "Update Car"
